e4e7cef069f775d7ee84f58cb974951a28539adb
ncorwins <ncorwins@iwu.edu>
2024-12-09 19:38:01 -0700

    idek help

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-        daisyui: {},
-    },
+    plugins: [require('tailwindcss'), require('autoprefixer')],
 };
